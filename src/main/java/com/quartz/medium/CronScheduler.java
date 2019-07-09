dd4a07242b964980b5187e10b7d5df49672b1d54
yuxiang123 <yuxiang@innjoy.me>
2019-07-09 09:59:57 +0800

    123

--- src/main/java/com/quartz/medium/CronScheduler.java
+++ src/main/java/com/quartz/medium/CronScheduler.java
@@ -9,7 +9,7 @@ public class CronScheduler {
         JobDetail jobDetail= JobBuilder.newJob(HelloTime.class).withIdentity("cronJob").build();
         //cronTrigger
         //每天9点45执行任务
-        CronTrigger cronTrigger= TriggerBuilder.newTrigger().withIdentity("cronTrigger").withSchedule(CronScheduleBuilder.cronSchedule("0 58 10 * * ?")).build();
+        CronTrigger cronTrigger= TriggerBuilder.newTrigger().withIdentity("cronTrigger").withSchedule(CronScheduleBuilder.cronSchedule("0 59 09 * * ?")).build();
         //1.每日10点15分触发      0 15 10 ？* *
         //2.每天下午的2点到2点59分（正点开始，隔5分触发）       0 0/5 14 * * ?
         //3.从周一到周五每天的上午10点15触发      0 15 10 ? MON-FRI
